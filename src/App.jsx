d638ca3c13f36c148c61f3475743edf03e482e18
leosum1t <pokharelsumit001@gmail.com>
2026-08-27 10:30:35 +0545

    feat: add application routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from "./pages/Home"
+import Explore from "./pages/Explore"
+import Favorites from "./pages/Favorites"
+import Dashboard from "./pages/Dashboard"
+import NotFound from "./pages/NotFound"
 
 function App() {
-  return <Home />
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/explore" element={<Explore />} />
+        <Route path="/favorites" element={<Favorites />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 export default App ;
